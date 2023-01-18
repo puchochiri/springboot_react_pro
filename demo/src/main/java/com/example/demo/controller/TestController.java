206971ce264e56880c4ba9121564acced10d9699
puchochiri <doawishfor@naver.com>
2023-01-18 22:59:02 +0900

    commit

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -2,9 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.TestRequestBodyDTO;
 
 @RestController
 @RequestMapping("test")
@@ -34,8 +37,11 @@ public class TestController {
 		return "Hello World! ID " + id; 
 	}
 	
+	
+	//localhost:8080/test/testRequestBody?id=123
 	@GetMapping("/testRequestBody")
 	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! ID" + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
 		
 	}
 	
